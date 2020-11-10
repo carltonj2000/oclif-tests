785a1152c657f60528b3de43deec4a917090afe4
Carlton Joseph <carlton.joseph@gmail.com>
2020-11-10 11:31:59 -0800

    Add use prompting with enquirer

--- packages/cli2/src/commands/init.ts
+++ packages/cli2/src/commands/init.ts
@@ -1,5 +1,6 @@
 import { Command, flags } from "@oclif/command";
 var debug = require("debug")("cli2:init");
+const { prompt } = require("enquirer");
 
 class Cli2 extends Command {
   static description = "describe the command here";
@@ -12,7 +13,6 @@ class Cli2 extends Command {
     name: flags.string({
       char: "n",
       description: "name to print",
-      default: "people",
     }),
     // flag with no value (-f, --force)
     force: flags.boolean({ char: "f" }),
@@ -25,6 +25,17 @@ class Cli2 extends Command {
     const { argv, args, flags } = this.parse(Cli2);
     debug("args", args);
     debug("argv", argv);
+    debug("flags", flags);
+    if (typeof flags.name === "undefined") {
+      flags.name = await prompt({
+        type: "input",
+        name: "name",
+        message: "What is your name?",
+      })
+        .then(({ name }: { name: string }) => name)
+        .catch(console.error)
+        .finally(() => console.log("name can also be specified via --name"));
+    }
     const name = flags.name ?? "world 2";
     this.log(`hello ${name} from ./src/index.ts`);
     if (args.file && flags.force) {
